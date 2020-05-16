34f03179e1bbac40cb39bfe581bdf29d0790d4aa
Erica <ericakong0331@gmail.com>
2020-05-16 02:41:10 -0700

    Updates

---
+++ precache-manifest.82f2d18a2ef5ad00257ac8e04ac19097.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf456ff8b95ee3ba760861dcdbcb95a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "506e1e7eb21dbf6ca9ab",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "def79ea28d97d810c635",
+    "url": "/static/js/2.c2fa3d69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2fa3d69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506e1e7eb21dbf6ca9ab",
+    "url": "/static/js/main.2367d99c.chunk.js"
+  },
+  {
+    "revision": "caf2a04453df4bace4e4",
+    "url": "/static/js/runtime-main.009173a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
